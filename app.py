cc78fa4d928f8f1d9e7918a20a4c9171beb31d5d
TANGUTURIVAMSHIKRISHNA <VAMSHI81000@GMAIL.COM>
2025-04-28 11:58:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import google.generativeai as genai
 
 
 #configure your Gemini API Key
-genai.configure(api_key="GOOGLE_API_KEY")
+genai.configure(api_key="AIzaSyDABqHCmisrnk98UCRYiXL_xIcOZwwS_P4")
 
 
 # Load the model
